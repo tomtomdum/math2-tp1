6af71f40b5a4944d3649a382def37e149f894919
tom <tommy.dumont@uqtr.ca>
2021-10-26 22:46:30 -0400

    algo fini??????????

--- src/com/company/graph/Route.java
+++ src/com/company/graph/Route.java
@@ -7,9 +7,7 @@ import java.util.Scanner;
 
 public class Route {
 
-    private Noeud noeudPrecedant;
     private Noeud noeudCourant;
-    private Noeud noeudSuivant;
 
     ArrayList<Noeud> listeNoeuds = new ArrayList<Noeud>();
     ArrayList<Arc> route = new ArrayList<Arc>();
@@ -18,41 +16,31 @@ public class Route {
         for(Noeud element : listeNoeuds) element.printer();
     }
 
-/*
-RESTE À FAIRE PEUT EN MANQUER
-* boucle
-* état final fait
-* chemin normal fait
-* chemin impossible
-*
-* */
-    public void runGraph(String serieBinaire){
-        Arc arcPrecedant = null;
+    /*
+     * chemin impossible
+     * */
+    public void runGraph(String serieBinaire) throws Exception {
         Arc aLoop = null;
+        Noeud noeudS =null;
         boolean thereIsALoop = false;
         int positionRoute = 0;
 
         // recherche du point de départ
         for(Noeud noeud: listeNoeuds){
             if (noeud.getNom().equals("S")) {
-                noeudCourant = noeud;
+                noeudCourant= noeudS = noeud;
                 break;
             }
         }
 
         for(int positionLangage = 0; positionLangage<serieBinaire.length(); positionLangage++){
             char ch = serieBinaire.charAt(positionLangage);
-//            noeudCourant = route.get(positionRoute).getDestination();
-//            noeudCourant.printer();
-
-            int positionArray = 0; // utiliser pour éliminer les chemin deja essayé
 
             // on détece si le noeud courant contient une boucle, car il faut la faire en premier
             for(Arc arc: noeudCourant.getListeDesArcs()){
                 if(arc.getSource().equals(arc.getDestination()) && Character.getNumericValue(ch) == arc.getValeurArc()) {
                     aLoop = arc;
                     thereIsALoop = true;
-                    System.out.println(arc.getSource().getNom() + "->" + arc.getValeurArc() + arc.getDestination().getNom());
                 }
             }
 
@@ -61,6 +49,7 @@ RESTE À FAIRE PEUT EN MANQUER
                     positionLangage++; // pas vraiment besoin d'assigner le noeud courant, cela revient au meme, on se déplace dans le langage donné
                     route.add(aLoop);
                     positionRoute++;// on ajoute seulement une loop dans le chemin utilisé
+                    System.out.println(aLoop.getSource().getNom() + "->" + aLoop.getValeurArc() + aLoop.getDestination().getNom());
                 }
                 thereIsALoop = false;
             }
@@ -76,29 +65,16 @@ RESTE À FAIRE PEUT EN MANQUER
                     System.out.println(arc.getSource().getNom() + "->" + arc.getValeurArc() + arc.getDestination().getNom());
                     break;
                 }
-                // peut etre inutile cette partie finalament??
-                //revenir en arriere
-//                if(positionArray == (noeudCourant.getListeDesArcs().size() -1)) {// le langage se rend nul part, on a atteint la fin de la liste d'arc
-//                    noeudCourant = route.get(positionRoute).getSource();// on retourne vers la source de l'arc, car on est déja à sa destination
-//                    // le noeud se rend nul part et n'est pas final, on enleve le chemin accessible
-//                    for(Noeud noeud: listeNoeuds){
-//                        if(noeud.getNom().equals(arc.getSource().getNom())) noeud.getListeDesArcs().remove(route.get(positionRoute)); positionRoute--;
-//                        break;
-//                    }
-//                    positionLangage--;
-//                }
-
-                positionArray++;
-
             }
 
+
             if(noeudCourant.isFinal() && positionLangage == (serieBinaire.length() - 1)){
-                System.out.println("Fin");
+                System.out.println("fIN");
                 for(Arc arc: route){
                     System.out.println(arc.getSource().getNom() + "->" + arc.getValeurArc() + arc.getDestination().getNom());
                 }
                 break;
-            } else if((positionLangage == serieBinaire.length() -1) && !noeudCourant.isFinal()){
+            } else if((positionLangage == serieBinaire.length() -1) && !noeudCourant.isFinal() || !possibilites(ch)){
                 positionRoute--;
                 noeudCourant = route.get(positionRoute).getSource();// on retourne vers la source de l'arc, car on est déja à sa destination
                 noeudCourant.removeArcObject(route.get(positionRoute));
@@ -107,13 +83,24 @@ RESTE À FAIRE PEUT EN MANQUER
 //                noeudCourant.removeArc(positionArray);
                 positionLangage -= 2; // on sosutrait deux car la boucle for ajoute un automatiquement
                 System.out.println("going back");
+            } else if( noeudCourant == noeudS && !possibilites(ch)){// on revient au point de départ et aucune possibilité n'est possible, donc le langage n'est pas accepté
+                throw new Exception("Le langage n'est pas possible");
             }
+
         }
     }
+public boolean possibilites(char ch){
+        for(Arc arc : noeudCourant.getListeDesArcs()){
+            if(Character.getNumericValue(ch) == arc.getValeurArc()){
+                return true;
+            }
+        }
+    return false;
+}
 
     public void fileParser(){
         try {
-            File myObj = new File("C:\\Users\\toys7\\Desktop\\math2-tp1\\src\\com\\company\\grammar.txt");
+            File myObj = new File("src/com/company/grammar.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
 
@@ -129,12 +116,12 @@ RESTE À FAIRE PEUT EN MANQUER
                 Noeud noeudFinal = new Noeud("final", true); // noeud générique à insérer dans un arc à chaque fois que la grammaire produit un état final ex: S->0
 
 
-                    String[] parts = data.split("->");
-                    lettreDuDepart = parts[0]; // va aller chercher premiere partie de l'expression
-                    goingto = parts[1];
-                    valeurDuChemin = goingto.replaceAll("[^0-9]+", "");
-                    goingto = parts[1];
-                    lettrePointee = goingto.replaceAll("\\d", "");
+                String[] parts = data.split("->");
+                lettreDuDepart = parts[0]; // va aller chercher premiere partie de l'expression
+                goingto = parts[1];
+                valeurDuChemin = goingto.replaceAll("[^0-9]+", "");
+                goingto = parts[1];
+                lettrePointee = goingto.replaceAll("\\d", "");
 
                 // recherche en premier si le noeud existe dans la liste
                 for (Noeud noeud : listeNoeuds) {
